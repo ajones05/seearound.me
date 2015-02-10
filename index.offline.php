98097ccfb10bc8c27b078e32b8827dfd779a963f
yuriyua <yuriyuag@gmail.com>
2015-02-10 20:19:06 +0200

    offline index page

---
+++ index.offline.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<style>
+		body {
+			color: #333333;
+			font-family: Arial,Helvetica,Sans Serif;
+			font-size: 78%;
+			margin: 0;
+			padding: 0;
+			text-align: center;
+		}
+
+		.outline {
+			background: #ffffff;
+			border: 1px solid #cccccc;
+			margin: 200px auto 20px;
+			padding: 20px;
+			width: 400px;
+		}
+	</style>
+</head>
+<body>
+	<div id="frame" class="outline">
+		<p>This site is down for maintenance.<br />Please check back again soon.</p>
+	</div>
+</body>
+</html>
