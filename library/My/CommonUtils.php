7caa270d974b9d2655e2ba246ea273e458d0f539
yuriyua <yuriyuag@gmail.com>
2015-03-25 20:03:34 +0200

    https://github.com/ajones05/seearound.me/issues/31#issuecomment-85713155

--- library/My/CommonUtils.php
+++ library/My/CommonUtils.php
@@ -5,16 +5,44 @@
 class My_CommonUtils
 {
 	/**
+	 * Replaces link to href.
 	 *
-	 *
-	 * @param	string	$text
+	 * @param	string	$body
 	 *
 	 * @return	string
 	 */
-	public static function linkClickable($text)
+	public static function linkClickable($body)
 	{
-		$text = preg_replace("/(?<!http:\/\/)www\./","http://www.", $text);
-		return preg_replace('!(((f|ht)tp://)[-a-zA-Z?-??-?()0-9@:%_+.~#?&;//=]+)!i', '<a href="$1" target="_blank">$1</a>', $text);
+		return preg_replace_callback(
+			// '/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/m',
+			'/((http(s)?:\/\/.)|(www\.))[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/m',
+			function($match)
+			{
+				$text = trim($match[0]);
+				$pieces = parse_url($text);
+				$scheme = isset($pieces['scheme']) ? $pieces['scheme'] : 'http';
+				$host = isset($pieces['host']) ? $pieces['host'] : $pieces['path'];
+				$link = $scheme . '://' . $host;
+
+				if (isset($pieces['path']) && $pieces['path'] != $host)
+				{
+					$link .= $pieces['path'];
+				}
+
+				if (isset($pieces['query']))
+				{
+					$link .= '?' . $pieces['query'];
+				}
+
+				if (isset($pieces['fragment']))
+				{
+					$link .= '#' . $pieces['fragment'];
+				}
+
+				return '<a href="' . htmlspecialchars($link) . '">' . $text . '</a>';
+			},
+			$body
+		);
 	}
 
 	/**
