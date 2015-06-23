3c9a3e11de73e9cc1cfda1cabbdc07661d7cee1f
yuriyua <yuriyuag@gmail.com>
2015-06-23 19:38:37 +0300

    #81 - restore "dateConversion" helper method (application/views/helpers/DateConversion.php:dateConversion => library/My/CommonUtils.php:dateConversion)

--- library/My/CommonUtils.php
+++ library/My/CommonUtils.php
@@ -111,4 +111,68 @@ class My_CommonUtils
         mt_srand();
         return substr(md5(mt_rand(0, time())), mt_rand(0, 20), 10);
     }
+
+	/**
+	 *
+	 *
+	 * @return	string
+	 */
+	public static function dateConversion($secountds = null)
+	{
+		if ($secountds)
+		{
+			$data_ref = date('Y-m-d H:i:s', $secountds);
+		}
+		else
+		{
+			$data_ref = date('Y-m-d H:i:s');
+		}
+
+		$current_date = date('Y-m-d H:i:s');
+		$diff = strtotime($current_date) - strtotime($data_ref);
+		$years = floor($diff / (365*60*60*24)); 
+		$months = floor(($diff - $years * 365*60*60*24) / (30*60*60*24));
+		$days = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
+		$hours = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24)/ (60*60));
+		$minuts = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60)/ 60);
+		$seconds = floor(($diff - $years * 365*60*60*24 - $months*30*60*60*24 - $days*60*60*24 - $hours*60*60 - $minuts*60));
+
+		if ($years > 0)
+		{
+			if ($years == '1')
+			{
+				return $years . ' Year';
+			}
+			else
+			{
+				return $years . ' Years';
+			}
+		}
+		elseif ($months > 0)
+		{
+			if ($years == '1')
+			{
+				return $months . ' Month';
+			}
+			else
+			{
+				return $months . ' Months';
+			}
+		}
+		elseif ($days > 0)
+		{
+			if ($days == '1')
+			{
+				return 'Yesterday';
+			}
+			else
+			{
+				return $days . ' Days';
+			}
+		}
+		else
+		{
+			return 'Today';
+		}
+	}
 }
