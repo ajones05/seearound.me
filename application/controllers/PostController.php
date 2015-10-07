c24a278b5a615e596c49eb3114e19e8ded67175d
yuriyua <yuriyuag@gmail.com>
2015-10-07 11:06:14 +0300

    #105 - posts list controller and view classes

---
+++ application/controllers/PostController.php
@@ -0,0 +1,67 @@
+<?php
+use Respect\Validation\Validator as v;
+
+/**
+ * Post controller class.
+ * Handles post actions.
+ */
+class PostController extends Zend_Controller_Action
+{
+	/**
+	 * Posts list action.
+	 *
+	 * @return void
+	 */
+	public function listAction()
+	{
+		$this->view->layout()->setLayout('posts');
+
+		$auth = Zend_Auth::getInstance()->getIdentity();
+		// TODO: remove
+		$auth['user_id'] = 276;
+
+		if (!Application_Model_User::checkId($auth['user_id'], $user))
+		{
+			throw new RuntimeException('You are not authorized to access this action');
+		}
+
+		$point = $this->_request->getParam('point');
+
+		if (!v::string()->latlng()->addOr(v::nullValue())->validate($point))
+		{
+			throw new RuntimeException('Incorrect point value: ' .
+				var_export($point, true));
+		}
+
+		$center = $this->_request->getParam('center');
+
+		if (!v::string()->latlng()->addOr(v::nullValue())->validate($center))
+		{
+			throw new RuntimeException('Incorrect center value: ' .
+				var_export($center, true));
+		}
+
+		$keywords = $this->_request->getParam('keywords');
+
+		if (!v::string()->addOr(v::nullValue())->validate($keywords))
+		{
+			throw new RuntimeException('Incorrect keywords value: ' .
+				var_export($keywords, true));
+		}
+
+		$mapCenter = $point ? explode(',', $point) :
+			($center ? explode(',', $center) : $user->location());
+
+		$posts = (new Application_Model_News)->search(array(
+			'keywords' => $keywords,
+			'latitude' => $mapCenter[0],
+			'longitude' => $mapCenter[1],
+			'radius' => $point ? 0.018939 : 0.8,
+			'limit' => 15
+		), $user);
+
+		$this->view->user = $user;
+		$this->view->posts = $posts;
+		$this->view->headScript()->appendScript('var mapCenter = ' . json_encode($mapCenter) . ';');
+	}
+}
