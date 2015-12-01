81e6d004b5bc967fffa6acf01973d5ad1060ec07
yuriyua <yuriyuag@gmail.com>
2015-12-01 12:52:10 +0200

    add mobile/request-nearest debug page

--- application/modules/admin/controllers/MobileApiController.php
+++ application/modules/admin/controllers/MobileApiController.php
@@ -69,4 +69,15 @@ class Admin_MobileApiController extends Zend_Controller_Action
 		$this->view->headScript()
 			->appendFile(My_Layout::assetUrl('www/scripts/mobile-api.js', $this->view));
     }
+
+	/**
+	 * Retrieve message conversation action.
+	 *
+	 * @return void
+	 */
+    public function requestNearestAction()
+    {
+		$this->view->headScript()
+			->appendFile(My_Layout::assetUrl('www/scripts/mobile-api.js', $this->view));
+    }
 }
