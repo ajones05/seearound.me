8a64cd18904189144aa2549fff4c69879604317e
yuriyua <yuriyuag@gmail.com>
2016-02-22 11:29:10 +0200

    fix mobile notifications query
    change query conditions for select notifications for last 15 days or not
    readed

--- application/controllers/MobileController.php
+++ application/controllers/MobileController.php
@@ -1901,7 +1901,8 @@ class MobileController extends Zend_Controller_Action
 				'user_name' => 'u.Name',
 				'is_read' => 'f.notify'
 			]);
-			$select1->where('f.reciever_id=? AND f.status=1 AND fl.created_at>=?', $user->id, $maxDate);
+			$select1->where('f.reciever_id=? AND f.status=1', $user->id);
+			$select1->where('f.notify=0 OR fl.created_at>=?', $maxDate);
 			$select1->joinLeft(['fl' => 'friend_log'],
 				'fl.friend_id=f.id AND fl.status_id=f.status', '');
 			$select1->joinLeft(['u' => 'user_data'], 'u.id=fl.user_id', '');
@@ -1916,7 +1917,8 @@ class MobileController extends Zend_Controller_Action
 				'user_name' => 'u.Name',
 				'is_read' => 'cm.is_read'
 			]);
-			$select2->where('cm.to_id=? AND cm.created_at>=?', $user->id, $maxDate);
+			$select2->where('cm.to_id=?', $user->id, $maxDate);
+			$select2->where('cm.is_read=0 OR cm.created_at>?', $maxDate);
 			$select2->joinLeft(['u' => 'user_data'], 'u.id=cm.from_id', '');
 
 			$userModel->setThumbsQuery($select2, [[320, 320]], 'u');
@@ -1932,7 +1934,8 @@ class MobileController extends Zend_Controller_Action
 			]);
 			$select3->where('n.isdeleted=0 AND n.user_id=?', $user->id);
 			$select3->joinLeft(['v' => 'votings'], 'v.news_id=n.id', '');
-			$select3->where('v.canceled=0 AND v.user_id<>? AND v.created_at>=?', $user->id, $maxDate);
+			$select3->where('v.canceled=0 AND v.user_id<>?', $user->id);
+			$select3->where('v.is_read=0 OR v.created_at>?', $maxDate);
 			$select3->joinLeft(['u' => 'user_data'], 'u.id=v.user_id', '');
 			$userModel->setThumbsQuery($select3, [[320, 320]], 'u');
 
@@ -1947,7 +1950,8 @@ class MobileController extends Zend_Controller_Action
 			]);
 			$select4->where('n.isdeleted=0 AND n.user_id=?', $user->id);
 			$select4->joinLeft(['c' => 'comments'], 'c.news_id=n.id', '');
-			$select4->where('c.isdeleted=0 AND c.user_id<>? AND c.created_at>=?', $user->id, $maxDate);
+			$select4->where('c.isdeleted=0 AND c.user_id<>?', $user->id);
+			$select4->where('c.is_read=0 OR c.created_at>?', $maxDate);
 			$select4->joinLeft(['u' => 'user_data'], 'u.id=c.user_id', '');
 			$userModel->setThumbsQuery($select4, [[320, 320]], 'u');
 
