bce983a58eb3df007fd09ae9a04d78100834ce05
yuriyua <yuriyuag@gmail.com>
2016-05-04 11:44:21 +0300

    #130 - remove mobile/edit-post api debug page

--- application/modules/admin/controllers/MobileApiController.php
+++ application/modules/admin/controllers/MobileApiController.php
@@ -214,17 +214,6 @@ class Admin_MobileApiController extends Zend_Controller_Action
     }
 
 	/**
-	 * Edit post action.
-	 *
-	 * @return void
-	 */
-    public function editPostAction()
-    {
-		$this->view->headScript()
-			->appendFile(My_Layout::assetUrl('www/scripts/mobile-api.js', $this->view));
-    }
-
-	/**
 	 * Save post action.
 	 *
 	 * @return void
