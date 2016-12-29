a731c7e7be222fa7737cd83de6a453790baa1044
yuriyua <yuriyuag@gmail.com>
2016-12-29 19:43:35 +0200

    add custom view path to helper

--- library/My/ViewHelper.php
+++ library/My/ViewHelper.php
@@ -7,15 +7,15 @@ class My_ViewHelper
 	/**
 	 * Processes a view script and returns the output.
 	 *
-	 * @param	string	$name
-	 * @param	array	$vars
-	 *
-	 * @return	string
+	 * @param string $name
+	 * @param array $vars
+	 * @param script $path
+	 * @return string
 	 */
-	public static function render($name, array $vars = array())
+	public static function render($name, array $vars=[], $path='views/scripts')
 	{
 		$view = new Zend_View;
-		$view->addScriptPath(APPLICATION_PATH . '/views/scripts');
+		$view->addScriptPath(APPLICATION_PATH . '/' . $path);
 		$view->assign($vars);
 
 		if (!preg_match('/\..+$/', $name))
