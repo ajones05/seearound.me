12d5095101b0a3a38bcb89a2fa9e8e8a9d001e66
yuriyua <yuriyuag@gmail.com>
2016-12-29 13:04:34 +0200

    add random post like bot

---
+++ cli/post-like-random.php
@@ -0,0 +1,109 @@
+<?php
+defined('ROOT_PATH')
+    || define('ROOT_PATH', dirname(dirname(__FILE__)));
+
+defined('ROOT_PATH_WEB') ||
+	define('ROOT_PATH_WEB', ROOT_PATH . '/web');
+
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(ROOT_PATH . '/application'));
+
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ?
+    getenv('APPLICATION_ENV') : 'production'));
+
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+require_once 'Zend/Application.php';
+
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+
+$application->bootstrap();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+set_time_limit(0);
+
+$voteModel = new Application_Model_Voting;
+$postModel = new Application_Model_News;
+
+$query = $postModel->select()->setIntegrityCheck(false)
+	->from('news', [
+		'news.id',
+		'news.vote',
+		'like_created_at' => 'pl.created_at'
+	])
+	// @INFO: available posts
+	->where('news.isdeleted=0')
+	// @INFO: interval 6 hours
+	->where('news.created_date>DATE_SUB(NOW(), INTERVAL 6 HOUR)')
+	// @INFO: the latest bot vote
+	->joinLeft(['pl' => 'votings'],
+		'(pl.news_id=news.id AND pl.user_id IS NULL AND pl.active=1)', '')
+	->group('news.id')
+	->order('RAND()');
+
+$limit = 100;
+$start = 0;
+
+do
+{
+	$posts = $postModel->fetchAll($query->limit($limit, $start));
+
+	if (($count = $posts->count()) == 0)
+	{
+		break;
+	}
+
+	foreach ($posts as $post)
+	{
+		if ($post['like_created_at'] != null)
+		{
+			$likeTime = strtotime($post['like_created_at']);
+
+			if ((time() - $likeTime) / 3600 < 1)
+			{
+				continue;
+			}
+		}
+
+		$voteCount = rand(0, 2);
+
+		if ($voteCount == 0)
+		{
+			continue;
+		}
+
+		$voteModel->update([
+			'updated_at' => new Zend_Db_Expr('NOW()'),
+			'active' => 0
+		], 'active=1 AND user_id IS NULL AND news_id=' . $post->id);
+
+		for ($i = 1; $i <= $voteCount; $i++)
+		{
+			$isActive = $i == $voteCount;
+
+			$voteModel->insert([
+				'vote' => 1,
+				'news_id' => $post->id,
+				'updated_at' => !$isActive ? new Zend_Db_Expr('NOW()') : null,
+				'active' => $isActive ? 1 : 0
+			]);
+
+			$post->vote++;
+		}
+
+		$postModel->update(['vote' => $post->vote], 'id=' . $post->id);
+
+		echo "POST #" . $post->id . " added " . $voteCount . " bot vote(s)\n";
+	}
+
+	$start += $limit;
+}
+while ($count >= $limit);
